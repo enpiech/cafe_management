f48130ff9ca892463dbac43a1c26fe8f68b40210
enpiech <enpiech@gmail.com>
2019-09-25 17:04:21 +0700

    Layout chung thêm, xóa, sửa: - Bàn - Danh mục - Menu Layout danh sách: - Bàn - Danh mục - Menu Layout login

--- app/src/main/java/com/example/cafemanagement/view/CreateTable.kt
+++ app/src/main/java/com/example/cafemanagement/view/CreateTable.kt
@@ -1,12 +1,13 @@
-package fit.tdc.edu.vn.cafemanagement
+package com.example.cafemanagement.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cafemanagement.R
 
-class MainActivity : AppCompatActivity() {
+class CreateTable : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_table)
     }
 }
---
+++ app/src/main/java/com/example/cafemanagement/view/ui/tablelist/TableListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cafemanagement.ui.tablelist
+
+import androidx.lifecycle.ViewModel
+
+class TableListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
