ad1dc0c5d301ab7656b412d2b2d307479466512a
Cao Thanh Lâm <32740259+litcao99@users.noreply.github.com>
2019-10-12 10:54:14 +0700

    Chỉnh sửa: - Thêm Factory của CategoryViewViewModel

--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/data/viewmodel/category_viewmodel/CategoryViewModelFactory.kt
+++ app/src/main/java/fit/tdc/edu/vn/cafemanagement/data/viewmodel/category_viewmodel/CategoryViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import fit.tdc.edu.vn.cafemanagement.data.data_source.firebase.FireBaseDataSource
 import fit.tdc.edu.vn.cafemanagement.data.repository.impl.CategoryRepository
 import fit.tdc.edu.vn.cafemanagement.ui.category_list.CategoryViewModel
+import fit.tdc.edu.vn.cafemanagement.ui.category_view.CategoryViewViewModel
 
 class CategoryViewModelFactory : ViewModelProvider.Factory {
 
@@ -12,6 +13,12 @@ class CategoryViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CategoryViewModel::class.java)) {
             return CategoryViewModel(
+                categoryRepository = CategoryRepository(
+                    dataSource = FireBaseDataSource()
+                )
+            ) as T
+        }else if(modelClass.isAssignableFrom(CategoryViewViewModel::class.java)) {
+            return CategoryViewViewModel(
                 categoryRepository = CategoryRepository(
                     dataSource = FireBaseDataSource()
                 )
