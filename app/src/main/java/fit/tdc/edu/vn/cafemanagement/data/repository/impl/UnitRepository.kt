ed13d499d1cf67d8f718d7ff5350c2d3cc5ce371
le tuan anh <tuananh0333@gmail.com>
2019-10-15 07:25:48 +0700

    some thing...

--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/data/repository/impl/UnitRepository.kt
+++ app/src/main/java/fit/tdc/edu/vn/cafemanagement/data/repository/impl/UnitRepository.kt
@@ -45,7 +45,7 @@ class UnitRepository : UnitRepositoryAPI {
 
     override fun getUnitsASC() : MediatorLiveData<List<Unit>?> {
         filteredUnitList.addSource(getAllUnits()) {}
-        filteredUnitList.value!!.sortedBy {
+        filteredUnitList.value = filteredUnitList.value!!.sortedBy {
             it.name
         }
         return filteredUnitList
@@ -53,7 +53,7 @@ class UnitRepository : UnitRepositoryAPI {
 
     override fun getUnitsDESC() : MediatorLiveData<List<Unit>?> {
         filteredUnitList.addSource(getAllUnits()) {}
-        filteredUnitList.value!!.sortedByDescending {
+        filteredUnitList.value = filteredUnitList.value!!.sortedByDescending {
             it.name
         }
         return filteredUnitList
