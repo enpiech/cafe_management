1f5b1e3fde01bb1566f4ad7a685f3b0ebe227b51
Cao Thanh Lâm <32740259+litcao99@users.noreply.github.com>
2019-10-28 03:55:22 +0700

    Fix 1 số lỗi hiển thị Table

--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/order_list/OrderListViewModel.kt
+++
@@ -1,7 +0,0 @@
-package fit.tdc.edu.vn.cafemanagement.fragment.order_list
-
-import androidx.lifecycle.ViewModel
-
-class OrderListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableViewFragment.kt
+++ app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableViewFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
+import android.widget.Spinner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
@@ -13,7 +14,10 @@ import androidx.navigation.fragment.navArgs
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import fit.tdc.edu.vn.cafemanagement.R
+import fit.tdc.edu.vn.cafemanagement.data.adapter.SpinnerZoneAdapter
+import fit.tdc.edu.vn.cafemanagement.data.adapter.SpinnerZoneHolder
 import fit.tdc.edu.vn.cafemanagement.data.adapter.TableAdapter
+import fit.tdc.edu.vn.cafemanagement.data.adapter.ZoneHolder
 import fit.tdc.edu.vn.cafemanagement.data.model.FormState
 import fit.tdc.edu.vn.cafemanagement.data.model.table.Table
 import fit.tdc.edu.vn.cafemanagement.data.model.table.TableViewFormState
@@ -27,7 +31,8 @@ import fit.tdc.edu.vn.cafemanagement.util.asEditText
 import kotlinx.android.synthetic.main.form_table.*
 
 class TableViewFragment : BaseViewFragment(R.layout.fragment_table_view) {
-    var viewAdapter = TableAdapter()
+
+    val spinnerAdapter = SpinnerZoneAdapter()
 
     companion object {
         fun newInstance() = TableViewFragment()
@@ -51,15 +56,45 @@ class TableViewFragment : BaseViewFragment(R.layout.fragment_table_view) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        spinnerAdapter.fetchSpinnerItems().observe(this, Observer {
+//            val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, it)
+//            table_spinner.adapter = spinnerAdapter
+//        })
+
+//        zoneViewModel.getAllItems().observe(this, Observer {
+////            val spinner = spinnerAdapter.submitList(it.data)
+////            table_spinner.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerAdapter.submitList(it.data))
+////            Log.d("test", "testtt "+it.data.toString())
+//            val options = arrayOf(Zone.apply {
+//                var zone:Zone
+//                return@apply
+//                zone.name
+//            })
+//
+//            val adapter = options?.let { it1 ->
+//                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,
+//                    it1
+//                )
+//            }
+//            table_spinner.adapter = adapter
+//        })
+
         zoneViewModel.getAllItems().observe(this, Observer {
-            val options = it.data
-            val spinnerAdapter = options?.let { it1 ->
-                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,
-                    it1
-                )
-            }
-            table_spinner.adapter = spinnerAdapter
+            spinnerAdapter.submitList(it.data)
+            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, spinnerAdapter.currentList)
+            Log.d("test", "casdads"+spinnerAdapter.currentList)
+            table_spinner.adapter = adapter
         })
+
+//        zoneViewModel.getAllItems().observe(this, Observer {
+//            val options = it.data
+//            val spinnerAdapter = options?.let { it1 ->
+//                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item,
+//                    it1
+//                )
+//            }
+//            table_spinner.adapter = spinnerAdapter
+//        })
 
 //        zoneViewModel.getAllItems().observe(this, Observer {
 //            val items = ArrayList<Zone>()
@@ -199,3 +234,4 @@ class TableViewFragment : BaseViewFragment(R.layout.fragment_table_view) {
         }
     }
 }
+
