f354a01220587eba8d546de8f9f0e458252f5463
enpiech <enpiech@gmail.com>
2019-10-30 15:42:32 +0700

    Fix: - TableDetail(View) spinner not showing items

--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableListWaiterFragment.kt
+++ app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableListWaiterFragment.kt
@@ -9,9 +9,8 @@ import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import fit.tdc.edu.vn.cafemanagement.R
 import fit.tdc.edu.vn.cafemanagement.data.adapter.TableWaiterAdapter
-import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table_viewmodel.TableDetailViewModel
-import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table_viewmodel.TableListViewModel
-import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table_viewmodel.TableViewModelFactory
+import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table.TableListViewModel
+import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table.TableViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_list.*
 
--- app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableViewFragment.kt
+++ app/src/main/java/fit/tdc/edu/vn/cafemanagement/fragment/table/TableViewFragment.kt
@@ -1,31 +1,21 @@
 package fit.tdc.edu.vn.cafemanagement.fragment.table
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.*
-import androidx.lifecycle.Observer
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
-import androidx.navigation.NavArgs
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import fit.tdc.edu.vn.cafemanagement.R
 import fit.tdc.edu.vn.cafemanagement.data.model.FormState
 import fit.tdc.edu.vn.cafemanagement.data.model.table.Table
 import fit.tdc.edu.vn.cafemanagement.data.model.table.TableViewFormState
-import fit.tdc.edu.vn.cafemanagement.data.model.zone.Zone
 import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table.TableDetailViewModel
 import fit.tdc.edu.vn.cafemanagement.data.viewmodel.table.TableViewModelFactory
 import fit.tdc.edu.vn.cafemanagement.fragment.BaseDetailViewModel
-import fit.tdc.edu.vn.cafemanagement.fragment.BaseViewFragment
 import fit.tdc.edu.vn.cafemanagement.fragment.BaseViewFragmentTest
 import fit.tdc.edu.vn.cafemanagement.util.asEditText
-import kotlinx.android.synthetic.main.dropdown_menu_popup_item.*
 import kotlinx.android.synthetic.main.form_table.*
-import kotlinx.android.synthetic.main.layout_tab_list.*
 
 class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_detail) {
     override val args by navArgs<TableViewFragmentArgs>()
@@ -39,7 +29,7 @@ class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_de
     override fun showError(state: FormState) {
         val tableFormState = state as TableViewFormState
         if (tableFormState.nameError != null) {
-            table_edit_name.error = getString(tableFormState.nameError!!)
+            table_edit_name.error = getString(tableFormState.nameError)
         } else {
             table_edit_name.error = null
             table_edit_name.isErrorEnabled = false
@@ -56,10 +46,6 @@ class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_de
             }
         }
 
-        if (type == FormState.Type.MODIFY) {
-            table_edit_name.editText?.isEnabled = false
-        }
-
         setupForm()
     }
 
@@ -71,6 +57,16 @@ class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_de
 
     override fun fillFormWith(item: Table) {
         table_edit_name.editText?.setText(item.name)
+        (viewModel as TableDetailViewModel).currentZone.observe(
+            viewLifecycleOwner,
+            androidx.lifecycle.Observer { czone ->
+                czone?.name?.let { name ->
+                    table_spinner.setText(name, false)
+                    (viewModel as TableDetailViewModel).currentZone.removeObservers(
+                        viewLifecycleOwner
+                    )
+                }
+            })
     }
 
     override fun setupForm() {
@@ -79,6 +75,28 @@ class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_de
                 getCurrentFormData()
             )
         }
+        (viewModel as TableDetailViewModel).zoneList.observe(
+            viewLifecycleOwner,
+            androidx.lifecycle.Observer {
+                if (it.isNotEmpty()) {
+                    val list = it.map { zone -> zone.name }
+                    table_spinner.setAdapter(
+                        ArrayAdapter(
+                            requireContext(),
+                            R.layout.dropdown_menu_popup_item,
+                            list
+                        )
+                    )
+                } else {
+                    if ((viewModel as TableDetailViewModel).currentZone.value != null) {
+                        // TODO notify no zone
+//                        table_spinner.setText(getText(R.string.warning_store_no_free_manager), false)
+                    } else {
+                        // TODO notify not set zone
+//                        table_spinner.setText(getText(R.string.warning_store_missing_manager), false)
+                    }
+                }
+            })
         table_spinner.setOnItemClickListener { parent, view, position, id ->
             viewModel.validate(
                 getCurrentFormData().apply {
@@ -93,6 +111,7 @@ class TableViewFragment : BaseViewFragmentTest<Table>(R.layout.fragment_table_de
     private fun enableForm(isEnabled: Boolean) {
         table_edit_name.editText?.isEnabled = isEnabled
         table_spinner.isEnabled = isEnabled
+        table_spinner_layout.isEndIconVisible = isEnabled
     }
 
 }
